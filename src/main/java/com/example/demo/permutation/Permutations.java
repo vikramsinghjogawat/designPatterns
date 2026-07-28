962b81a3ce7f04f512950aeeb651fcb424938f46
Vikram Singh Jogawat <vikramsinghjogawat5@gmail.com>
2026-07-28 21:14:51 +0530

    Added permuation logic with backTracking

---
+++ src/main/java/com/example/demo/permutation/Permutations.java
@@ -0,0 +1,26 @@
+package com.example.demo.designPatterns.permutation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+
+    //using backtracking
+    public static void main(String[] args) {
+        String input = "abc";
+        List<String > res = new ArrayList<>();
+        permute(res,"",input);
+        System.out.println(res);// [abc, acb, bac, bca, cab, cba]
+    }
+
+    private static void permute(List<String> res, String current, String remaining) {
+        if(remaining.isEmpty()){
+            res.add(current);
+            return;
+        }
+        for(int i=0;i<remaining.length();i++){
+            String newRemaining =remaining.substring(0,i)+remaining.substring(i+1);
+            permute(res,current+remaining.charAt(i),newRemaining);
+        }
+    }
+}
